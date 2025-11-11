6050740250838b8f531edb7b3f10d05af5a6f674
austin wamono <austinwamono98@gmail.com>
2025-11-11 23:11:23 +0300

    commit#2Branch1:instances of the JComponets,constructor having our jframe and load methods

--- src/main/java/UsingPDF.java
+++ src/main/java/UsingPDF.java
@@ -9,4 +9,14 @@ import java.util.Date;
 import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.*;
 public class UsingPDF {
+    JFrame Mainframe;
+    JPanel holdingPanel;
+    JTable TabularDataTable;
+    JButton ExportToPDF;
+    DefaultTableModel tableModel;
+    public UsingPDF(){
+        this.prepareJFrame();
+        //incase you have not created the database comment this method to just see empty table
+        this.loadProductsForTheTable();
+    }
 }
